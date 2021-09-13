5444239c803706a89d7e9f9c2065736708584ebc
Glenn Gunter <gunterglenn33@gmail.com>
2021-09-13 10:15:33 +0200

    finished index, fixing show

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,10 @@
 class PagesController < ApplicationController
   def home
+    @shoes = Shoe.all
+  end
+
+  def show
+    @shoe = Shoe.find(id: params[:id])
+    # @variant = Variant.
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,25 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+
+puts 'Destroying all'
+Shoe.destroy_all
+Variant.destroy_all
+
+
+puts 'Generating shoes..'
+shoe1 = Shoe.create(name: 'Adidas', description: 'This are Adidas shoes')
+shoe2 = Shoe.create(name: 'Nike', description: 'This are Nike shoes')
+shoe3 = Shoe.create(name: 'DC', description: 'This are DC shoes')
+shoe4 = Shoe.create(name: 'Puma', description: 'This are Puma shoes')
+shoe5 = Shoe.create(name: 'Reebok', description: 'This are Reebok shoes')
+
+puts 'Generating shoes variants..'
+Variant.create(name: 'Talle 41', price: 15, shoe_id: shoe1.id )
+Variant.create(name: 'Talle 41', price: 15, shoe_id: shoe2.id )
+Variant.create(name: 'Talle 41', price: 15, shoe_id: shoe3.id )
+Variant.create(name: 'Talle 41', price: 15, shoe_id: shoe4.id )
+Variant.create(name: 'Talle 41', price: 15, shoe_id: shoe5.id )
+Variant.create(name: 'Talle 38', price: 33, shoe_id: shoe1.id)
+Variant.create(name: 'Talle 38', price: 33, shoe_id: shoe2.id)
+Variant.create(name: 'Talle 38', price: 33, shoe_id: shoe3.id)
+Variant.create(name: 'Talle 38', price: 33, shoe_id: shoe4.id)
+Variant.create(name: 'Talle 38', price: 33, shoe_id: shoe5.id)
+
